7e003190d7b7fa89a8baa70c15703d62fffef8ba
Your Name <you@example.com>
2023-12-11 14:36:22 +0800

    asdasdas

--- src/containers/todo/Navbar.js
+++ src/containers/todo/Navbar.js
@@ -1,13 +1,13 @@
 import "./todo.css";
 
-import { FaList } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
 
 export default function Navbar({ user, logout }) {
   return (
     <nav className="navbar">
       <div className="nav-logo">
-        <FaList size={20} />
-        <h1>BadList</h1>
+        <FaStar size={20} />
+        <h1>NotDo</h1>
       </div>
       <div className="nav-links">
         <p>{user.email}</p>
